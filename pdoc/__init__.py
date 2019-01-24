1d62d77c9f5c7fadf78ec80958192a247b13c936
Kernc <kerncece@gmail.com>
2019-01-24 17:09:17 +0100

    DOC: Mention final Python 2 version (0.3.x)

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -301,6 +301,9 @@ modified templates into the `directories` list of the
 Compatibility
 -------------
 `pdoc` requires Python 3.5+.
+The last version to support Python 2.x is [pdoc3 0.3.x].
+
+[pdoc3 0.3.x]: https://pypi.org/project/pdoc3/0.3.11/
 
 
 Contributing
