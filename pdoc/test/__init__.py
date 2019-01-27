pdoc3/pdoc

commit e2739ced41b66450138c5372378ded8543b9c309

ENH: Support 2-space indents in Google docstring format
Fixes https://github.com/pdoc3/pdoc/issues/5

pdoc/html_helpers.py
@@ -172,10 +172,10 @@ class _ToMarkdown:
     @staticmethod
     def google(text,
                _googledoc_sections=partial(
-                   re.compile(r'(?<=\n\n)(\w+):$\n((?:\n?(?: {4}.*|$))+)', re.MULTILINE).sub,
+                   re.compile(r'(?<=\n\n)(\w+):$\n((?:\n?(?: {2,}.*|$))+)', re.MULTILINE).sub,
                    lambda m, _params=partial(
                            re.compile(r'^([\w*]+)(?: \(([\w. ]+)\))?: '
-                                      r'((?:.*)(?:\n(?: {4}.*|$))*)', re.MULTILINE).sub,
+                                      r'((?:.*)(?:\n(?: {2,}.*|$))*)', re.MULTILINE).sub,
                            lambda m: _ToMarkdown._deflist(*m.groups())): (
                        '\n{}\n-----\n{}'.format(
                            m.group(1), _params(inspect.cleandoc(m.group(2))))))):

pdoc/test/example_pkg/__init__.py
@@ -166,9 +166,9 @@ class Docformats:
             ValueError: If `arg2` is equal to `arg1`.
 
         Examples:
-            Examples in doctest format.
+          Examples in doctest format.
 
-            >>> a = [1,2,3]
+          >>> a = [1,2,3]
 
         Todos:
             * For module TODOs

commit 65c2513c93c336379fcc9962427da9120b932471

BUG: Make deflists always indented 4 spaces
Fix bug introduced in:
e2739ce ENH: Support 2-space indents in Google docstring format

pdoc/html_helpers.py
@@ -124,6 +124,7 @@ class _ToMarkdown:
         """
         type = _type_parts(type or '')
         desc = desc or '&nbsp;'
+        assert _ToMarkdown._is_indented_4_spaces(desc)
         if type:
             return '**`{}`** :&ensp;{}\n:   {}\n\n'.format(name, type, desc)
         return '**`{}`**\n:   {}\n\n'.format(name, desc)
@@ -170,13 +171,24 @@ class _ToMarkdown:
         return text
 
     @staticmethod
+    def _is_indented_4_spaces(txt, _3_spaces_or_less=re.compile(r'\n\s{0,3}\S').search):
+        return '\n' not in txt or not _3_spaces_or_less(txt)
+
+    @staticmethod
+    def _fix_indent(name, type, desc):
+        """Mazbe fix indent from 2 to 4 spaces."""
+        if not _ToMarkdown._is_indented_4_spaces(desc):
+            desc = desc.replace('\n', '\n  ')
+        return name, type, desc
+
+    @staticmethod
     def google(text,
                _googledoc_sections=partial(
                    re.compile(r'(?<=\n\n)(\w+):$\n((?:\n?(?: {2,}.*|$))+)', re.MULTILINE).sub,
                    lambda m, _params=partial(
                            re.compile(r'^([\w*]+)(?: \(([\w. ]+)\))?: '
                                       r'((?:.*)(?:\n(?: {2,}.*|$))*)', re.MULTILINE).sub,
-                           lambda m: _ToMarkdown._deflist(*m.groups())): (
+                           lambda m: _ToMarkdown._deflist(*_ToMarkdown._fix_indent(*m.groups()))): (
                        '\n{}\n-----\n{}'.format(
                            m.group(1), _params(inspect.cleandoc(m.group(2))))))):
         """

pdoc/test/__init__.py
@@ -660,6 +660,15 @@ Nomatch:</p>
 <dd>Description of arg1</dd>
 <dt><strong><code>arg2</code></strong> :&ensp;<code>str</code> or <code>int</code></dt>
 <dd>Description of arg2</dd>
+<dt><strong><code>test_sequence</code></strong></dt>
+<dd>
+<p>2-dim numpy array of real numbers, size: N * D
+- the test observation sequence.</p>
+<pre><code>test_sequence =
+code
+</code></pre>
+<p>Continue.</p>
+</dd>
 <dt><strong><code>*args</code></strong></dt>
 <dd>passed around</dd>
 </dl>

pdoc/test/example_pkg/__init__.py
@@ -151,9 +151,16 @@ class Docformats:
         Nomatch:
 
         Args:
-            arg1 (int): Description of arg1
-            arg2 (str or int): Description of arg2
-            *args: passed around
+          arg1 (int): Description of arg1
+          arg2 (str or int): Description of arg2
+          test_sequence: 2-dim numpy array of real numbers, size: N * D
+            - the test observation sequence.
+
+                test_sequence =
+                code
+
+            Continue.
+          *args: passed around
 
         Returns:
             bool: Description of return value