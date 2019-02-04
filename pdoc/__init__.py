9de4ee36bb05df69f0599ea2e8033d6014b07d08
Kernc <kerncece@gmail.com>
2019-02-04 13:14:48 +0100

    fixup! DOC: Reword, link to further info

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -184,7 +184,7 @@ Supported docstring formats
 ---------------------------
 Currently, pure Markdown (with [extensions]), [numpydoc],
 and [Google-style] docstrings formats are supported,
-along with some reST directives..
+along with some reST directives.
 
 *[reST]: reStructuredText
 [extensions]: https://python-markdown.github.io/extensions/#officially-supported-extensions
@@ -194,20 +194,20 @@ along with some reST directives..
 
 ### Supported reST directives
 
-The following reST directives are supported:
+The following reST directives should work:
 
 * specific and generic [admonitions],
-* [`.. image::`](http://docutils.sourceforge.net/docs/ref/rst/directives.html#images)
-  or `.. figure::` (without options),
-* [`.. include::`](http://docutils.sourceforge.net/docs/ref/rst/directives.html#images),
-  with support for the options: `:start-line:`, `:end-line:`, `:start-after:`
-  and `:end-before:`.
+* [`.. image::`][image] or `.. figure::` (without options),
+* [`.. include::`][include], with support for the options:
+  `:start-line:`, `:end-line:`, `:start-after:` and `:end-before:`.
 * `.. versionadded::`
 * `.. versionchanged::`
 * `.. deprecated::`
 * `.. todo::`
 
 [admonitions]: http://docutils.sourceforge.net/docs/ref/rst/directives.html#admonitions
+[image]: http://docutils.sourceforge.net/docs/ref/rst/directives.html#images
+[include]: http://docutils.sourceforge.net/docs/ref/rst/directives.html#including-an-external-document-fragment
 
 
 Linking to other identifiers
