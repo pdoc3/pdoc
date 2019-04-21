3701f6d65d65966305e18ada6016dc10d0679edb
Kernc <kerncece@gmail.com>
2019-04-21 14:00:21 +0200

    MNT: Add adsense code to generated docs

--- doc/build.sh
+++ doc/build.sh
@@ -32,6 +32,9 @@ if [ "$IS_RELEASE" ]; then
     ANALYTICS="<script>window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;ga('create','UA-43663477-5','auto');ga('require','cleanUrlTracker',{indexFilename:'index.html',trailingSlash:'add'});ga('require','outboundLinkTracker',{events:['click','auxclick','contextmenu']});ga('require','maxScrollTracker');ga('require','pageVisibilityTracker');ga('send','pageview');setTimeout(function(){ga('send','event','pageview','view')},15000);</script><script async src='https://www.google-analytics.com/analytics.js'></script><script async src='https://cdnjs.cloudflare.com/ajax/libs/autotrack/2.4.1/autotrack.js'></script>"
     find "$BUILDROOT" -name '*.html' -print0 |
         xargs -0 -- sed -i "s#</body>#$ANALYTICS</body>#i"
+    ANALYTICS='<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><script>(adsbygoogle=window.adsbygoogle||[]).push({google_ad_client:"ca-pub-2900001379782823",enable_page_level_ads:true});</script>'
+    find "$BUILDROOT" -name '*.html' -print0 |
+        xargs -0 -- sed -i "s#</head>#$ANALYTICS</head>#i"
 fi
 
 
