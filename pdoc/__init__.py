b2753146bc2cb2487323b14299bec9b0610197d7
Chad Smith <cs01@users.noreply.github.com>
2019-04-30 19:50:00 -0700

    move intro paragraph back into source code

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -1,4 +1,10 @@
 """
+Python package `pdoc` provides types, functions, and a command-line
+interface for accessing public documentation of Python modules, and
+for presenting it in a user-friendly, industry-standard open format.
+It is best suited for small- to medium-sized projects with tidy,
+hierarchical APIs.
+
 .. include:: ./documentation.md
 """
 import ast
