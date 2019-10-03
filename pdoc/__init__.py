pdoc3/pdoc

commit 148237b14625edcd5456c9abd45e3ec89de7f157

BUG: HTML-escape function parameter default values
Such as `object()` rendering as `<object ...`, breaking HTML.

Tentatively fixes https://github.com/pdoc3/pdoc/issues/108.

pdoc/__init__.py
@@ -1100,10 +1100,21 @@ class Function(Doc):
             return ["..."]
 
         def safe_default_value(p: inspect.Parameter):
+            if p.default is inspect.Parameter.empty:
+                return p
+
+            replacement = None
             if p.default is os.environ:
+                replacement = 'os.environ'
+            elif inspect.isclass(p.default):
+                replacement = p.default.__module__ + '.' + p.default.__qualname__
+            elif ' at 0x' in repr(p.default):
+                replacement = re.sub(r' at 0x\w+', '', repr(p.default))
+
+            if replacement:
                 class mock:
                     def __repr__(self):
-                        return 'os.environ'
+                        return replacement
                 return p.replace(default=mock())
             return p
 

pdoc/templates/html.mako
@@ -94,7 +94,7 @@
             if returns:
                 returns = ' ->\N{NBSP}' + returns
         %>
-        <span>${f.funcdef()} ${ident(f.name)}</span>(<span>${params})${returns}</span>
+        <span>${f.funcdef()} ${ident(f.name)}</span>(<span>${params | h})${returns | h}</span>
     </code></dt>
     <dd>${show_desc(f)}</dd>
   </%def>

pdoc/test/__init__.py
@@ -130,6 +130,7 @@ class CliTest(unittest.TestCase):
 
     def test_html(self):
         include_patterns = [
+            'a=&lt;object',
             'CONST docstring',
             'var docstring',
             'foreign_var',
@@ -162,6 +163,7 @@ class CliTest(unittest.TestCase):
             ' class="ident">static',
         ]
         exclude_patterns = [
+            '<object ',
             ' class="ident">_private',
             ' class="ident">_Private',
         ]
@@ -264,6 +266,7 @@ class CliTest(unittest.TestCase):
 
     def test_text(self):
         include_patterns = [
+            'object_as_arg_default(*args, a=<object ',
             'CONST docstring',
             'var docstring',
             'foreign_var',
@@ -614,6 +617,9 @@ class ApiTest(unittest.TestCase):
         func = pdoc.Function('f', mod,
                              lambda a=os.environ: None)
         self.assertEqual(func.params(), ['a=os.environ'])
+
+        func = pdoc.Function('f', mod, lambda a=object(): None)
+        self.assertEqual(func.params(), ['a=<object object>'])
 
         # typed
         def f(a: int, *b, c: typing.List[pdoc.Doc] = []): pass

pdoc/test/example_pkg/__init__.py
@@ -18,7 +18,10 @@ __pdoc__ = {}
 
 def foo(env=os.environ):
     """Doesn't leak environ"""
-    pass
+
+
+def object_as_arg_default(*args, a=object(), **kwargs):
+    """Html-encodes angle brackets in params"""
 
 
 class A:

commit b832209a3f8b3939ba128827237adc11e62b46c1

BUG: Fix types regression introduced in 148237b14625edcd5456c9abd45e3ec89de7f157
Fixes https://github.com/pdoc3/pdoc/issues/114

pdoc/__init__.py
@@ -1104,6 +1104,11 @@ class Function(Doc):
             elif ' at 0x' in repr(p.default):
                 replacement = re.sub(r' at 0x\w+', '', repr(p.default))
 
+            nonlocal link
+            if link and ('<' in repr(p.default) or '>' in repr(p.default)):
+                import html
+                replacement = html.escape(replacement or p.default)
+
             if replacement:
                 class mock:
                     def __repr__(self):

pdoc/templates/html.mako
@@ -104,7 +104,7 @@
             if returns:
                 returns = ' ->\N{NBSP}' + returns
         %>
-        <span>${f.funcdef()} ${ident(f.name)}</span>(<span>${params | h})${returns | h}</span>
+        <span>${f.funcdef()} ${ident(f.name)}</span>(<span>${params})${returns}</span>
     </code></dt>
     <dd>${show_desc(f)}</dd>
   </%def>

pdoc/test/__init__.py
@@ -639,6 +639,9 @@ class ApiTest(unittest.TestCase):
         func = pdoc.Function('f', mod, lambda a=object(): None)
         self.assertEqual(func.params(), ['a=<object object>'])
 
+        func = pdoc.Function('f', mod, lambda a=object(): None)
+        self.assertEqual(func.params(link=lambda x: ''), ['a=&lt;object object&gt;'])
+
         # typed
         def f(a: int, *b, c: typing.List[pdoc.Doc] = []): pass
         func = pdoc.Function('f', mod, f)