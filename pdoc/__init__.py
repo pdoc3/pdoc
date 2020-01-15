6604a51205ce44c9886d834c46aeffd2dc09d8be
Kernc <kerncece@gmail.com>
2020-01-15 01:35:08 +0100

    BUG: Document compiled/bytecode submodules
    Fixes https://github.com/pdoc3/pdoc/issues/132

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -39,8 +39,6 @@ _URL_MODULE_SUFFIX = '.html'
 _URL_INDEX_MODULE_SUFFIX = '.m.html'  # For modules named literal 'index'
 _URL_PACKAGE_SUFFIX = '/index.html'
 
-_SOURCE_SUFFIXES = tuple(importlib.machinery.SOURCE_SUFFIXES)
-
 T = TypeVar('T', bound='Doc')
 
 __pdoc__ = {}  # type: Dict[str, Union[bool, str]]
@@ -166,10 +164,13 @@ def import_module(module, *, reload: bool = False) -> ModuleType:
     """
     @contextmanager
     def _module_path(module):
-        from os.path import isfile, isdir, split, abspath, splitext
-        path, module = '_pdoc_dummy_nonexistent', module
-        if isdir(module) or isfile(module) and module.endswith(_SOURCE_SUFFIXES):
-            path, module = split(splitext(abspath(module))[0])
+        from os.path import abspath, basename, dirname, isfile, isdir
+        path = '_pdoc_dummy_nonexistent'
+        module_name = inspect.getmodulename(module)
+        if isdir(module):
+            path, module = dirname(abspath(module)), basename(module)
+        elif isfile(module) and module_name:
+            path, module = dirname(abspath(module)), module_name
         try:
             sys.path.insert(0, path)
             yield module
@@ -569,13 +570,14 @@ class Module(Doc):
                 because that one doesn't play well with namespace packages.
                 See: https://github.com/pypa/setuptools/issues/83
                 """
-                from os.path import isdir, join, splitext
+                from os.path import isdir, join
                 for pth in paths:
                     for file in os.listdir(pth):
                         if file.startswith(('.', '__pycache__', '__init__.py')):
                             continue
-                        if file.endswith(_SOURCE_SUFFIXES):
-                            yield splitext(file)[0]
+                        module_name = inspect.getmodulename(file)
+                        if module_name:
+                            yield module_name
                         if isdir(join(pth, file)) and '.' not in file:
                             yield file
 
