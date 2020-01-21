d73a992c40432d1783c74ccab89344a2226a9670
Frank Stettner <frank-stettner@gmx.net>
2020-01-21 15:00:30 +0100

    ENH: Better parsing of markdown code spans. Fix issue #145.

--- pdoc/html_helpers.py
+++ pdoc/html_helpers.py
@@ -405,7 +405,7 @@ def to_markdown(text: str, docformat: str = 'numpy,google', *,
                 # E.g. `code` and [foo is `bar`]() but not [`code`](...)
                 # Also skips \-escaped grave quotes.
                 _code_refs=re.compile(r'(?<![\\`])`(?:[^\]`]|(?<=\\)'
-                                      r'`(?!](.*)))+`(?!\](?:: )?\(.*\))').sub):
+                                      r'`(?!](.*)))+`(?!\](?:: +|\()[\w\./]*)').sub):
     """
     Returns `text`, assumed to be a docstring in `docformat`, converted to markdown.
 
