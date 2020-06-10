2f994a2dbede625d4f87f7eb51059343b674c325
Lluis <l.salord.quetglas@gmail.com>
2020-06-10 16:20:21 +0200

    Fix on issue #213

--- pdoc/html_helpers.py
+++ pdoc/html_helpers.py
@@ -573,6 +573,7 @@ def _git_project_root():
                 ['git', 'rev-parse', '--show-toplevel']):
         try:
             path = subprocess.check_output(cmd, universal_newlines=True).rstrip('\r\n')
+            path = os.path.normpath(path)
             if path:
                 break
         except (subprocess.CalledProcessError, OSError):
@@ -591,7 +592,7 @@ def _project_relative_path(absolute_path):
     for prefix_path in (_git_project_root() or os.getcwd(),
                         get_python_lib()):
         common_path = os.path.commonpath([prefix_path, absolute_path])
-        if common_path == prefix_path:
+        if os.path.samefile(common_path, prefix_path):
             # absolute_path is a descendant of prefix_path
             return os.path.relpath(absolute_path, prefix_path)
     raise RuntimeError(
