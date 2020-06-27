pdoc3/pdoc

commit cee9dab88f8fb1eb205274cb9853639d362e051b

BUG: Avoid extracting doctest comments into sidebar TOC
Fixes https://github.com/pdoc3/pdoc/issues/194

pdoc/html_helpers.py
@@ -348,6 +348,8 @@ class _ToMarkdown:
     def _directive_opts(text: str) -> dict:
         return dict(re.findall(r'^ *:([^:]+): *(.*)', text, re.MULTILINE))
 
+    DOCTESTS_RE = re.compile(r'^(?:>>> .*)(?:\n.+)*', re.MULTILINE)
+
     @staticmethod
     def doctests(text):
         """
@@ -355,7 +357,7 @@ class _ToMarkdown:
         doctest blocks so they render as Python code.
         """
         with _fenced_code_blocks_hidden(text) as result:
-            result[0] = re.compile(r'^(?:>>> .*)(?:\n.+)*', re.MULTILINE).sub(
+            result[0] = _ToMarkdown.DOCTESTS_RE.sub(
                 lambda match: '```python\n' + match.group() + '\n```\n', result[0])
         text = result[0]
         return text
@@ -533,6 +535,7 @@ def extract_toc(text: str):
     """
     Returns HTML Table of Contents containing markdown titles in `text`.
     """
+    text = _ToMarkdown.DOCTESTS_RE.sub('', text)
     toc, _ = _md.reset().convert('[TOC]\n\n@CUT@\n\n' + text).split('@CUT@', 1)
     if toc.endswith('<p>'):  # CUT was put into its own paragraph
         toc = toc[:-3].rstrip()

pdoc/test/__init__.py
@@ -1080,7 +1080,7 @@ pdoc
         self.assertIn('example_pkg.nonexisting', cm.warning.args[0])
 
     def test_extract_toc(self):
-        text = 'xxx\n\n# Title\n\nfoo\n\n## Subtitle\n\nbar'
+        text = 'xxx\n\n# Title\n\nfoo\n\n## Subtitle\n\nbar\n\n>>> # doctests skipped'
         expected = '''<div class="toc">
 <ul>
 <li><a href="#title">Title</a><ul>

commit 448bf078867533eeb6d7164ec12167f6f87b5e0e

BUG: Fix extracting TOC with interleaved doctests in code spans
Fixes: https://github.com/pdoc3/pdoc/issues/223
Regression since: https://github.com/pdoc3/pdoc/commit/cee9dab88f8fb1eb205274cb9853639d362e051b

pdoc/html_helpers.py
@@ -535,7 +535,9 @@ def extract_toc(text: str):
     """
     Returns HTML Table of Contents containing markdown titles in `text`.
     """
-    text = _ToMarkdown.DOCTESTS_RE.sub('', text)
+    with _fenced_code_blocks_hidden(text) as result:
+        result[0] = _ToMarkdown.DOCTESTS_RE.sub('', result[0])
+    text = result[0]
     toc, _ = _md.reset().convert('[TOC]\n\n@CUT@\n\n' + text).split('@CUT@', 1)
     if toc.endswith('<p>'):  # CUT was put into its own paragraph
         toc = toc[:-3].rstrip()

pdoc/test/__init__.py
@@ -1080,11 +1080,37 @@ pdoc
         self.assertIn('example_pkg.nonexisting', cm.warning.args[0])
 
     def test_extract_toc(self):
-        text = 'xxx\n\n# Title\n\nfoo\n\n## Subtitle\n\nbar\n\n>>> # doctests skipped'
+        text = '''xxx
+
+# Title
+
+>>> # doctests skipped
+# doctest output, skipped
+
+## Subtitle
+
+```
+>>> # code skipped
+# skipped
+```
+
+# Title 2
+
+```
+>>> # code skipped
+# skipped
+```
+
+## Subtitle 2
+'''
         expected = '''<div class="toc">
 <ul>
 <li><a href="#title">Title</a><ul>
 <li><a href="#subtitle">Subtitle</a></li>
+</ul>
+</li>
+<li><a href="#title-2">Title 2</a><ul>
+<li><a href="#subtitle-2">Subtitle 2</a></li>
 </ul>
 </li>
 </ul>