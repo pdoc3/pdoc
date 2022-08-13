7a70257c81c46bcdddec800946d4ce0473ad1b5b
Abhinav Singh <mailsforabhinav@gmail.com>
2022-08-13 21:23:26 +0530

    Skip `__editable__` paths during `iter_modules`

--- pdoc/__init__.py
+++ pdoc/__init__.py
@@ -727,6 +727,8 @@ class Module(Doc):
                 """
                 from os.path import isdir, join
                 for pth in paths:
+                    if pth.startswith("__editable__"):
+                        continue
                     for file in os.listdir(pth):
                         if file.startswith(('.', '__pycache__', '__init__.py')):
                             continue
