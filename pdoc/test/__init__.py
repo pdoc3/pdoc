da13281edcee60329867438d2e57a2ac66a51178
Kernc <kerncece@gmail.com>
2024-03-09 06:02:22 +0100

    MNT: Fix flake8 errors

--- pdoc/test/__init__.py
+++ pdoc/test/__init__.py
@@ -250,13 +250,13 @@ class CliTest(unittest.TestCase):
             )
 
     def test_docformat(self):
-        with self.assertWarns(UserWarning) as cm,\
+        with self.assertWarns(UserWarning) as cm, \
                 run_html(EXAMPLE_MODULE, config='docformat="restructuredtext"'):
             self._basic_html_assertions()
         self.assertIn('numpy', cm.warning.args[0])
 
     def test_html_no_source(self):
-        with self.assertWarns(DeprecationWarning),\
+        with self.assertWarns(DeprecationWarning), \
                 run_html(EXAMPLE_MODULE, html_no_source=None):
             self._basic_html_assertions()
             self._check_files(exclude_patterns=['class="source"', 'Hidden'])
@@ -301,7 +301,7 @@ class CliTest(unittest.TestCase):
             self._basic_html_assertions()
             self._check_files(exclude_patterns=['<a href="/sys.version.ext"'])
 
-        with self.assertWarns(DeprecationWarning),\
+        with self.assertWarns(DeprecationWarning), \
                 run_html(EXAMPLE_MODULE, external_links=None):
             self._basic_html_assertions()
             self._check_files(['<a title="sys.version" href="/sys.version.ext"'])
@@ -319,7 +319,7 @@ class CliTest(unittest.TestCase):
             pdoc.tpl_lookup._collection.clear()
 
     def test_link_prefix(self):
-        with self.assertWarns(DeprecationWarning),\
+        with self.assertWarns(DeprecationWarning), \
                 run_html(EXAMPLE_MODULE, link_prefix='/foobar/'):
             self._basic_html_assertions()
             self._check_files(['/foobar/' + EXAMPLE_MODULE])
@@ -445,8 +445,8 @@ class CliTest(unittest.TestCase):
                 self._check_files(())
 
     def test_skip_errors(self):
-        with chdir(os.path.join(TESTS_BASEDIR, EXAMPLE_MODULE, '_skip_errors')),\
-                redirect_streams(),\
+        with chdir(os.path.join(TESTS_BASEDIR, EXAMPLE_MODULE, '_skip_errors')), \
+                redirect_streams(), \
                 self.assertWarns(pdoc.Module.ImportWarning) as cm:
             run('.', skip_errors=None)
         self.assertIn('ZeroDivision', cm.warning.args[0])
